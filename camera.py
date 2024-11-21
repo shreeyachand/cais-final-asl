f66341eca00eb48797c272841339ee0700d97941
Shreeya Chand <chshreeya@gmail.com>
2024-11-21 09:50:19 -0800

    list of classes in order

--- camera.py
+++ camera.py
@@ -1,4 +1,10 @@
 import torch
 
+
 model = torch.load('5-epochs.pth', weights_only=False)
-model
+
+classes = list('ABCDEFGHIJKLMNOPQRSTUVWYXZ')
+classes.append('del')
+classes.append('nothing')
+classes.append('space')
+# model outputs an number 0-28, which corresponds to the index of the label in this classes list.
